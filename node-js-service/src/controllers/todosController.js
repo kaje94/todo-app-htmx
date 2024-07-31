f47464b7e01d73fb1aa8a3f9d4404dd29f394967
kaje94 <a.kajendran@gmail.com>
2024-07-31 16:49:20 +0530

    add go connection to nodejs

--- node-js-service/src/controllers/todosController.js
+++ node-js-service/src/controllers/todosController.js
@@ -1,70 +1,93 @@
-const TodoModel = require('../models/todoModel');
+const TodoModel = require("../models/todoModel");
+import oauth from "axios-oauth-client";
+import axios from "axios";
+
+const serviceURL = process.env.SVC_URL;
+
+const getClientCredentials = oauth.clientCredentials(
+  axios.create(),
+  process.env.TOKEN_URL,
+  process.env.CONSUMER_KEY,
+  process.env.CONSUMER_SECRET
+);
 
 class TodosController {
   // Get all todos
   async getAll(req, res) {
     try {
-      console.log('Running "getAll" ', process.env.ENV1)
+      console.log('Running "getAll" ');
       const todos = await TodoModel.findAll();
+
+      const auth = await getClientCredentials();
+      const accessToken = auth.access_token;
+      const response = await axios.get(`${serviceURL}/todos`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+      console.log("response:", response);
+
       res.status(200).json(todos);
     } catch (error) {
-      res.status(500).json({ message: 'Error getting todos' });
+      res.status(500).json({ message: "Error getting todos" });
     }
   }
 
   // Get a single todo by id
   async getById(req, res) {
     try {
-      console.log('Running "getById"')
+      console.log('Running "getById"');
       const todo = await TodoModel.findById(req.params.id);
       if (todo) {
         res.status(200).json(todo);
       } else {
-        res.status(404).json({ message: 'Todo not found' });
+        res.status(404).json({ message: "Todo not found" });
       }
     } catch (error) {
-      res.status(500).json({ message: 'Error getting todo' });
+      res.status(500).json({ message: "Error getting todo" });
     }
   }
 
   // Create a new todo
   async create(req, res) {
     try {
-      console.log('Running "create"')
+      console.log('Running "create"');
       const todo = await TodoModel.create(req.body);
       res.status(201).json(todo);
     } catch (error) {
-      res.status(500).json({ message: 'Error creating todo' });
+      res.status(500).json({ message: "Error creating todo" });
     }
   }
 
   // Update a todo
   async update(req, res) {
     try {
-      console.log('Running "update"')
+      console.log('Running "update"');
       const todo = await TodoModel.update(req.params.id, req.body);
       if (todo) {
         res.status(200).json(todo);
       } else {
-        res.status(404).json({ message: 'Todo not found' });
+        res.status(404).json({ message: "Todo not found" });
       }
     } catch (error) {
-      res.status(500).json({ message: 'Error updating todo' });
+      res.status(500).json({ message: "Error updating todo" });
     }
   }
 
   // Delete a todo
   async delete(req, res) {
     try {
-      console.log('Running "delete"')
+      console.log('Running "delete"');
       const todo = await TodoModel.destroy({ where: { id: req.params.id } });
       if (todo) {
-        res.status(200).json({ message: 'Todo deleted' });
+        res.status(200).json({ message: "Todo deleted" });
       } else {
-        res.status(404).json({ message: 'Todo not found' });
+        res.status(404).json({ message: "Todo not found" });
       }
     } catch (error) {
-      res.status(500).json({ message: 'Error deleting todo, ' + error?.message });
+      res
+        .status(500)
+        .json({ message: "Error deleting todo, " + error?.message });
     }
   }
 }
